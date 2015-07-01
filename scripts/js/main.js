9789d0b9725b4488e87bf5fb7bb30396699de89a
Nicolas Fourré <nicolas.fourre@ext.leroymerlin.fr>
2015-07-01 02:03:41 +0200

    Initial commit

---
+++ scripts/js/main.js
@@ -0,0 +1,68 @@
+/*
+* Fullscreen Function to adapt section to screen resolution
+ */
+function fullscreen(){
+    var screenHeight = window.innerHeight;
+    var fulldiv = document.querySelectorAll('.fullscreen');
+    for (var i = 0; i < fulldiv.length; i++) {
+        fulldiv[i].style.height = screenHeight+"px";
+    }
+}
+
+/*
+* Smooth Scroll Function
+ */
+function currentYPosition() {
+    // Firefox, Chrome, Opera, Safari
+    if (self.pageYOffset) return self.pageYOffset;
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+}
+
+function elmYPosition(eID) {
+    console.log(eID);
+    var elm = document.getElementById(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
+    } return y;
+}
+
+function smoothScroll(eID) {
+    var startY = currentYPosition();
+    var stopY = elmYPosition(eID);
+    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+        scrollTo(0, stopY); return;
+    }
+    var speed = Math.round(distance / 4);
+    console.log(speed);
+    if (speed >= 8) speed = 8;
+    var step = Math.round(distance / 25);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+        for ( var i=startY; i<stopY; i+=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+        } return;
+    }
+    for ( var i=startY; i>stopY; i-=step ) {
+        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fullscreen();
+
+    window.addEventListener('resize', fullscreen, false);
+});
